17d3daf7bb34e9cc5533c0e1388b1a0c69575201
arcas <arcascb2001@gmail.com>
2022-10-29 18:23:49 +0200

    added linia vista por roberto

--- src/Engine.py
+++ src/Engine.py
@@ -83,6 +83,7 @@ class GraphicsEngine:
 
         if keys[pg.K_r]:
             self.scene.new_road()
+            self.car.move_to_start()
         if keys[pg.K_UP]:
             self.car.move_forward()
         if keys[pg.K_RIGHT]:

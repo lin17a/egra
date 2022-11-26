247989454a1795078d21df1f0acfdc269561cc4b
arnaucasbas <arcascb2001@gmail.com>
2022-11-26 14:11:31 +0100

    fixed light

--- src/Light.py
+++ src/Light.py
@@ -4,10 +4,10 @@ class Light:
     def __init__(self, m, position=(0,30,0)):
         self.position = glm.vec3(position)
 
-        light_colos = {"sunset" : glm.vec3((251, 144, 98)),
-                        "desert" : glm.vec3((250, 213, 165)),
+        light_colos = {"sunset" : glm.vec3((251/255, 144/255, 98/255)),
+                        "desert" : glm.vec3((250/255, 213/255, 165/255)),
                         "field" : glm.vec3((1,1,1))}
-                        
+
         self.color = light_colos[m]
         # intensities
         self.Ia = 0.2 * self.color # ambient

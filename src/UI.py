76deb16876253b1747f10a60376ea709bd407314
Roberto Manresa <you@example.com>
2022-12-04 17:40:29 +0100

    Mejorando aparencia menu

--- src/UI.py
+++ src/UI.py
@@ -10,7 +10,8 @@ class menu:
         self.map = "field"
         font = pygame_menu.font.FONT_MUNRO
         mytheme = Theme(background_color=(0, 0, 0, 0), 
-                        widget_font=font)
+                        widget_font=font, 
+                        widget_font_size=50)
         myimage = pygame_menu.baseimage.BaseImage(
                     image_path=r"./textures/sb.jpg",
                     drawing_mode=pygame_menu.baseimage.IMAGE_MODE_REPEAT_XY
@@ -19,12 +20,15 @@ class menu:
         self.menu = pygame_menu.Menu(   height = 960, 
                                         theme = mytheme, 
                                         title = 'Race Driving Simulator', 
-                                        width = 1280
+                                        width = 1280, 
+                                        center_content=False
                                     )
-        self.menu.add.selector('Players:', ["1", "2"], onchange=self.set_player)
-        self.menu.add.selector('Maps:', [("Field", 1), ("Sunset", 2), ("Desert", 3)], onchange=self.set_map)
-        self.menu.add.button('Start', self.start)
-        self.menu.add.button('Quit', pygame_menu.events.EXIT)
+        X = 0
+        Y = 85
+        self.menu.add.selector('Players:', ["1", "2"], onchange=self.set_player).translate(X, Y)
+        self.menu.add.selector('Maps:', [("Field", 1), ("Sunset", 2), ("Desert", 3)], onchange=self.set_map).translate(X, Y)    
+        self.menu.add.button('Start', self.start).translate(X, Y)
+        self.menu.add.button('Quit', pygame_menu.events.EXIT).translate(X, Y)
 
     def set_player(self, players):
         self.players = int(players[0])
